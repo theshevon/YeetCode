7f7a8b4580eeb63d136c5845456332a5621f1b36
karlflores <karl.flores.9@gmail.com>
2019-03-17 04:16:18 +1100

    added json func

--- public/scripts/python/script.py
+++ public/scripts/python/script.py
@@ -1,7 +1,1 @@
-def recur(i):
-    if i <= 0:
-        return 1
-    print(i)
-    return recur(i-1)+recur(i-2);
-    
-print(recur(10))
+print("yeet")
--- routes/pythonprocessing.js
+++ routes/pythonprocessing.js
@@ -1,16 +1,23 @@
 const {PythonShell} = require('python-shell')
 const fs = require('fs')
-const file_path = 'public/scripts/python/script.py';
+const file_path = 'public/scripts/python/';
 // save as a python script
-const save_script = (script) => fs.writeFileSync(file_path,script);
+const save_script = (script) => fs.writeFileSync(file_path+'script.py',script);
 
 const run_script = (callback) => {
-    const syntax_check = PythonShell.checkSyntaxFile(file_path)
+    const syntax_check = PythonShell.checkSyntaxFile(file_path+'script.py')
 
     // check the syntax, if there is no error, then we can proceed to run the code
     syntax_check.then( (result)=>{
 
-        const pyshell = new PythonShell(file_path)
+        // get the ast and convert to a json object -- accessible via
+        // callback
+        const obj = ast_json(console.log)
+
+        return
+
+        // run the script
+        const pyshell = new PythonShell(file_path+'script.py')
 
 
         // run the script and return the output as an object
@@ -27,9 +34,32 @@ const run_script = (callback) => {
     })
 }
 
+const ast_json = (callback) => {
+    // run the python script to convert the python
+    const pyshell = new PythonShell(file_path+'astjson.py')
+    let message = ''
+
+    pyshell.on('message',(data)=>{
+        // then we want to aggregate the entire message
+        //console.log(data)
+        message+=data
+    })
+
+    pyshell.end((err,code,sig) => {
+        if(err){
+            callback(err)
+        }
+        json_ast = JSON.parse(message);
+        console.log(json_ast.body)
+        callback(json_ast)
+    })
+
+}
+
 module.exports = {
     save_script,
-    run_script
+    run_script,
     //
     //verify_script,
+    ast_json
 }
