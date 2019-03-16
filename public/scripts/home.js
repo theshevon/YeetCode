ecdb43dce93f7cca9ee22c0258fb7ce8ebca56a8
Shevon Mendis <shevonmendis98@gmail.com>
2019-03-16 19:29:15 +1100

    home and exercise pages

---
+++ public/scripts/home.js
@@ -0,0 +1,13 @@
+var prev_time = new Date().getTime();
+
+// while (true){
+
+//     var curr_time = new Date().getTime();
+//     if (curr_time - prev_time >= 1){
+//         prev_time = curr_time;
+//         $("#blinking").toggleClass("blink");
+//     }
+//     // blink();
+// }
+
+
